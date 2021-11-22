8434e1bdb4d48327192d604c792ba2d74163acef
RoberioG <robingp45@gmail.com>
2021-11-22 19:27:43 -0300

    Teste Controle
    Co-authored-by: Moisés Oliveira Costa <moisesocosta@users.noreply.github.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,54 @@
-function login(email, senha){
-    email = prompt("Digite seu email:");
-    senha = prompt("Digite sua senha:");
+function alertaUsuario(texto){
+    alert(texto);
+}
+
+var email = document.getElementById('email');
+var password = document.getElementById('password');
+email.addEventListener('focus', ()=>{
+    email.style.borderColor = '#4A5F6A';
+})
+email.addEventListener('blur', ()=>{
+    email.style.borderColor = '#ccc';
+})
+password.addEventListener('focus', ()=>{
+    password.style.borderColor = '#4A5F6A';
+})
+password.addEventListener('blur', ()=>{
+    password.style.borderColor = '#ccc';
+})
+
+/*
+let time = 2000, 
+    currentImageIndex = 0, 
+    images = document
+                .querySelectorAll(#slides img);
+    max = images.length;
+
+function nextImage(){
+    images[currentImageIndex].classList.remove("selected");
+
+    currentImageIndex++
+
+    if(currentImageIndex >= max){
+        currentImageIndex = 0;
+    }
+
+    images[currentImageIndex].classList.add("selected");
+}
+
+function star(){
+    setInterval(() => {
+        nextImage();
+    }, time);
+}
+
+window.addEventListener("load", start);
+*/
+
+/*
+document.getElementById("submitBtn").addEventListener("Entrar", myFunction);
+function myFunction(){
+    window.location.href="redirecionamento.html"
 }
 
 function mostrarSenha(){
@@ -11,7 +59,4 @@ function mostrarSenha(){
         senha.type = "password";
     }
 }
-
-function alertaUsuario(texto){
-    alert(texto);
-}
+*/

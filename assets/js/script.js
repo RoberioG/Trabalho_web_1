629618fbc51de385360efa36bca81ee8a32a04e2
Moisés Oliveira Costa <60359281+moisesocosta@users.noreply.github.com>
2021-11-22 14:32:02 -0300

    funções novas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,26 @@
-function login(email, senha){
-    email = prompt("Digite seu email:");
-    senha = prompt("Digite sua senha:");
+function alertaUsuario(texto){
+    alert(texto);
+}
+
+var email = document.getElementById('email');
+var password = document.getElementById('password');
+email.addEventListener('focus', ()=>{
+    email.style.borderColor = '#4A5F6A';
+})
+email.addEventListener('blur', ()=>{
+    email.style.borderColor = '#ccc';
+})
+password.addEventListener('focus', ()=>{
+    password.style.borderColor = '#4A5F6A';
+})
+password.addEventListener('blur', ()=>{
+    password.style.borderColor = '#ccc';
+})
+
+/*
+document.getElementById("submitBtn").addEventListener("Entrar", myFunction);
+function myFunction(){
+    window.location.href="redirecionamento.html"
 }
 
 function mostrarSenha(){
@@ -11,7 +31,4 @@ function mostrarSenha(){
         senha.type = "password";
     }
 }
-
-function alertaUsuario(texto){
-    alert(texto);
-}
+*/
